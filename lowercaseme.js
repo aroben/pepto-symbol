82b0d1af1d3334fe515a33337133fd6c43cf404a
Adam Roben <adam@github.com>
2012-04-16 17:07:38 -0400

    Initial commit

---
+++ lowercaseme.js
@@ -0,0 +1,17 @@
+var HTTP = require('http')
+  , HTTPProxy = require('http-proxy')
+  , URL = require('url')
+  ;
+
+var proxy = new HTTPProxy.RoutingProxy();
+
+HTTP.createServer(function(request, response) {
+  var innerURL = request.url.substr(1);
+  var parsedURL = URL.parse(innerURL);
+  request.url = innerURL.toLowerCase();
+
+  proxy.proxyRequest(request, response, {
+    host: parsedURL.hostname,
+    port: 80,
+  });
+}).listen(8001);
