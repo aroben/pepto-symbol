35771dd7d1718c5618c1ddd32334337264617dd2
Cheng Zhao <zcbenz@gmail.com>
2014-11-10 16:55:02 +0800

    Update to latest http-proxy.

--- pepto-symbol.js
+++ pepto-symbol.js
@@ -1,11 +1,16 @@
-const HTTPProxy = require('http-proxy');
+var http = require('http');
+var httpProxy = require('http-proxy');
 
 const TARGET_HOST = process.env.S3_BUCKET + '.s3.amazonaws.com';
+const TARGET = 'http://' + TARGET_HOST;
+
+// Create a proxy server with custom application logic.
+var proxy = httpProxy.createProxyServer({});
 
 // S3 returns 403 errors for files that don't exist. But when symsrv.dll sees a
 // 403 it blacklists the server for the rest of the debugging session. So we
 // convert 403s to 404s so symsrv.dll doesn't freak out.
-function convert403To404(request, response, next) {
+proxy.on('proxyReq', function(proxyReq, request, response, options) {
   var original = response.writeHead;
   response.writeHead = function() {
     var args = Array.prototype.slice.call(arguments);
@@ -13,23 +18,22 @@ function convert403To404(request, response, next) {
       args[0] = 404;
     original.apply(response, args);
   };
-  next();
-}
+});
 
 // S3 determines the bucket from the Host header.
-function fixHostHeader(request, response, next) {
-  request.headers['host'] = TARGET_HOST;
-  next();
-}
+proxy.on('proxyReq', function(proxyReq, request, response, options) {
+  proxyReq.setHeader('Host', TARGET_HOST);
+});
 
 // symstore.exe and symsrv.dll don't always agree on the case of the path to a
 // given symbol file. Since S3 URLs are case-sensitive, this causes symbol
 // loads to fail. To get around this, we assume that the symbols were uploaded
 // to S3 with all-lowercase keys, and we lowercase all requests we receive to
 // match.
-function lowercaseURL(request, response, next) {
-  request.url = request.url.toLowerCase();
-  next();
-}
+proxy.on('proxyReq', function(proxyReq, request, response, options) {
+  proxyReq.url = request.url.toLowerCase();
+});
 
-HTTPProxy.createServer(TARGET_HOST, 80, convert403To404, fixHostHeader, lowercaseURL).listen(process.env.PORT);
+http.createServer(function(request, response) {
+  proxy.web(request, response, { target: TARGET });
+}).listen(process.env.PORT);
